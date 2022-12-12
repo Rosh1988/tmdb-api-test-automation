9815df08e5f3a892c5de47bb574632e61ac6d67d
rosh90 <erroshni1988@gmail.com>
2022-12-12 18:39:31 +0100

    Updated tests

--- tests/test_movie.py
+++ tests/test_movie.py
@@ -2,18 +2,19 @@ from testhelpers import get_guest_session_id, get_api, put_api
 
 valid_api_key = '85e760da8dc8acd92ab9b3feaae90e20'
 invalid_api_key = '45sddrewfsdf8acd92ab9b3fasdsd34'
+valid_movie_id = '2'
+invalid_movie_id = '1'
 
 # Perform a GET request to /movie/top_rated with a valid API Key
 # Check that the response status code equals 200
-def test_get_top_rated_movies_check_status_code_equals_200():
+def test_get_top_rated_movies_status_code_200():
     status_code, response_data = get_api('3/movie/top_rated', valid_api_key)
     assert status_code == 200
-
 
 # Perform a GET request to /movie/top_rated with an invalid API Key
 # Check that the response status code equals 200
 # Check that the response returns a valid status_message
-def test_get_top_rated_movies_check_status_code_equals_401():
+def test_get_top_rated_movies_status_code_401():
     status_code, response_data = get_api('3/movie/top_rated', invalid_api_key)
     assert status_code == 401
     assert response_data['status_message'] == 'Invalid API key: You must be granted a valid key.'
@@ -23,7 +24,7 @@ def test_get_top_rated_movies_check_status_code_equals_401():
 # Perform a PUT request to /movie/{movie_id}/rating with a valid API Key & guest_session_id
 # Check that the response status code equals 201
 # Check that the response returns a valid status_message
-def test_put_rate_movie_check_status_code_equals_201():
+def test_put_rate_movie_status_code_201():
     data='{"value": 8.5}'
     guest_id = get_guest_session_id('3/authentication/guest_session/new', valid_api_key)
     status_code, response_data = put_api('3/movie/2/rating', valid_api_key, guest_id, data)
@@ -32,11 +33,25 @@ def test_put_rate_movie_check_status_code_equals_201():
 
 # Fetch a guest_session_id
 # Perform a PUT request to /movie/{movie_id}/rating with a an invalid API Key & guest_session_id
+# Provide an invalid API key
+# Provide a valid movie id
 # Check that the response status code equals 401
-# Check that the response returns a valid status_message
-def test_put_rate_movie_check_status_code_equals_401():
+# Check that the response returns a correct status_message
+def test_put_rate_movie_status_code_401():
     data='{"value": 8.5}'
     guest_id = get_guest_session_id('3/authentication/guest_session/new', valid_api_key)
     status_code, response_data = put_api('3/movie/2/rating', invalid_api_key, guest_id, data)
     assert status_code == 401
     assert response_data['status_message'] == 'Invalid API key: You must be granted a valid key.'
+
+# Fetch a guest_session_id
+# Perform a PUT request to /movie/{movie_id}/rating
+# Provide invalid movie id and valid API key
+# Check that the response status code equals 404
+# Check that the response returns a correct status_message
+def test_put_rate_movie_status_code_404():
+    data='{"value": 8.5}'
+    guest_id = get_guest_session_id('3/authentication/guest_session/new', valid_api_key)
+    status_code, response_data = put_api('3/movie/{invalid_movie_id}/rating', valid_api_key, guest_id, data)
+    assert status_code == 404
+    assert response_data['status_message'] == 'The resource you requested could not be found.'
